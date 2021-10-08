207c3284c8e374ed77a4863734bf9bc2aec06aaf
AlexxalexS <37020080+AlexxalexS@users.noreply.github.com>
2021-10-09 00:22:51 +0300

    Initial Commit

---
+++ FinGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinGame
+//
+//  Created by Alexey on 09.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinGame/FinGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinGameApp.swift
+//  FinGame
+//
+//  Created by Alexey on 09.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct FinGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
